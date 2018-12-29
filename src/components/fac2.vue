1f30e92576766b93aaae5b7c42d3aeab5072ba9b
badrf <s.s.f.b@hotmail.fr>
2018-12-29 11:09:10 +0100

    facs pages

---
+++ src/components/fac2.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-table striped hover :items="items" :fields="fields"></b-table>
+</template>
+
+<!-- ============================================================================ -->
+
+<script>
+import my_json from './../../server/test1.json'
+export default {
+  data () {
+    return {
+      // Note 'isActive' is left out and will not appear in the rendered table
+      fields: {
+        name: {
+          label: 'name',
+          sortable: true
+        },
+        surname: {
+          label: 'surname',
+          sortable: false
+        },
+        float: {
+          label: 'moyenne'
+        },
+        'email': {
+          label: 'e-mail'
+        }
+      },
+      items: my_json.items
+    }
+  }
+}
+</script>
