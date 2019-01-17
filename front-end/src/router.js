831c54a79c5382d53700a8f65ae9296edfd7cd66
badrf <s.s.f.b@hotmail.fr>
2019-01-17 18:53:27 +0100

    redirect login

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import Router from 'vue-router';
 import HomePage from './components/HomePage.vue';
 import List from './components/List.vue';
@@ -7,9 +8,10 @@ import ufs from './components/ufs.vue';
 import profile from './components/profile.vue';
 import Form from './components/addStudent.vue';
 import Form2 from './components/updateStudent.vue';
-import LoginComponent from './components/login.vue';
+//import LoginComponent from './components/login.vue';
 
-export default new Router({
+Vue.use(Router)
+let router = new Router({
 	mode: 'history',
 	routes: [
 		/*{path: '/', 
@@ -19,18 +21,34 @@ export default new Router({
 },*/
 	//component: HomePage},
 	{path: '/', 
-		name: "login",
-		component: LoginComponent
+		name: "Home",
+		component: HomePage
 	},
 	{path: '/Home',
 		name: 'Home',
 		component: HomePage
+		
 	},
 	{path: '/ufs/:id1?/list/:id2?', component: List},
 	{path: '/campus', component: Campus},
 	{path: '/stat', component: Stat},
 	{path: '/ufs/:id1?/list/:id2?/profile/:id3?', component: profile},
 	{path: '/ufs/:id?', component: ufs},
-	{path: '/ufs/:id1?/list/:id2?/form', component: Form},
-	{path: '/ufs/:id1?/list/:id2?/update/:id3?', component: Form2}]
+	{path: '/ufs/:id1?/list/:id2?/form', component: Form,meta: {
+		requiresAuth: true
+}},
+	{path: '/ufs/:id1?/list/:id2?/update/:id3?', component: Form2,meta: {
+		requiresAuth: true
+}}]
 });
+
+router.beforeEach((to, from, next) => {
+	if(to.matched.some(record => record.meta.requiresAuth)) {
+			next({
+				path: '/login',
+				//params: { nextUrl: to.fullPath }
+			})
+		
+}
+})
+export default router
